6f5d27a5d98d61233ada628799c11f00213c434f
Gabe <gabrielmscabera@gmail.com>
2016-04-07 17:12:23 -0500

    added view controllers

---
+++ RaffleHat/CreateEventViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateEventViewController.swift
+//  RaffleHat
+//
+//  Created by Gabe on 4/7/16.
+//  Copyright © 2016 GabeCabrera. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+    
+    @IBOutlet weak var EventImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
