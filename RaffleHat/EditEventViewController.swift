f42a23379a5483a244f56537cea05dac462524f8
Gabe <gabrielmscabera@gmail.com>
2016-04-08 10:40:04 -0500

    added UI Items for CreateEventViewController

---
+++ RaffleHat/EditEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditEventViewController.swift
+//  RaffleHat
+//
+//  Created by Gabe on 4/8/16.
+//  Copyright © 2016 GabeCabrera. All rights reserved.
+//
+
+import UIKit
+
+class EditEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
